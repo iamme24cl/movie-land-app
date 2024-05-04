04026b247ffd2de521daadd1324d07f06392a6a0
Chandra Lama <iamme24cl@gmail.com>
2024-05-04 12:11:56 -0700

    add rows component

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { baseUrl } from '../api/constants';
+import requests from "../api/requests";
 import './App.css';
-import Nav from "./Nav"
-import Banner from "./Banner"
+import Nav from "./Nav";
+import Banner from "./Banner";
+import Row from "./Row";
 
 const App = () => {
   const [personalizeUrl, setPersonalizeUrl] = useState(baseUrl + "/all");
@@ -14,12 +16,43 @@ const App = () => {
       setPersonalizeUrl(url);
     }
     console.log(personalizeUrl);
-  });
+  }, [loggedIn, personalizeUrl]);
 
   return (
     <div className='App'>
       <Nav />
       <Banner />
+
+      <Row 
+        title="Our Pick"
+        id="MVL"
+        fetchUrl={requests.fetchMVLPick}
+      />
+      <Row 
+        title="Recommended For You"
+        id="RF"
+        fetchUrl={personalizeUrl}
+      />
+      <Row 
+        title="Action Movies"
+        id="AM"
+        fetchUrl={requests.fetchActionMovies}
+      />
+      <Row 
+        title="Comedy Movies"
+        id="CM"
+        fetchUrl={requests.fetchComedyMovies}
+      />
+      <Row 
+        title="Horror Movies"
+        id="HM"
+        fetchUrl={requests.fetchHorrorMovies}
+      />
+      <Row 
+        title="Romance Movies"
+        id="RM"
+        fetchUrl={requests.fetchRomanceMovies}
+      />
     </div>
   );
 
--- src/components/Row.js
+++ src/components/Row.js
@@ -2,77 +2,83 @@ import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
 import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
 import React, { useEffect, useState } from "react";
 import axios from "../api/axios";
+import MovieModal from "./modal";
 import "./Row.css";
 
 const Row = ({ title, fetchUrl, id }) => {
-    const base_url = "https://image.tmdb.org/t/p/original/";
-    const [movies, setMovies] = useState([]);
-    const [modalVisibility, setModalVisibility] = useState(false);
-    const [movieSelected, setMovieSelection] = useState({});
+  const base_url = "https://image.tmdb.org/t/p/original/";
+  const [movies, setMovies] = useState([]);
+  const [modalVisibility, setModalVisibility] = useState(false);
+  const [movieSelected, setMovieSelection] = useState({});
 
-    useEffect(() => {
-        console.log(id, fetchUrl)
+  useEffect(() => {
+    console.log(id, fetchUrl);
+    //if [], run once when the row loads, and dont run again
 
-        async function fetchData() {
-            // Wait until we get the API response
-            const request = await axios.get(fetchUrl, {
-                headers: {
-                    "Access-Control-Allow-Origin": "*",
-                },
-            });
-            setMovies(request.data.result);
-            return request;
-        }
-        fetchData()
-    }, [fetchUrl]);
+    async function fetchData() {
+      //Dont move until we get the API answer
+      const request = await axios.get(fetchUrl, {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
+      });
+      setMovies(request.data.result);
+      return request;
+    }
 
-    const handleClick = (movie) => {
-        setModalVisibility(true);
-        setMovieSelection(movie)
-    };
+    fetchData();
+  }, [fetchUrl, id]);
 
-    return (
-        <section className="row">
-            <h2>{title}</h2>
-            <div className="slider">
-                <div className="slider__arrow-left">
-                    <span
-                        className="arrow"
-                        onClick={() => {
-                            document.getElementById(id).scrollLeft -= window.innerWidth - 80;
-                        }}
-                    >
-                        <ArrowBackIosIcon />
-                    </span>
-                </div>
-                <div className="row__posters">
-                    {movies.map((movie, idx) => {
-                        <img 
-                            key={idx}
-                            onClick={() => handleClick(movie)}
-                            className={`row__ poster row__posterLarge`}
-                            src={`${base_url}${movie.poster_path}`}
-                            loading="lazy"
-                            alt={movie.title}
-                        />
-                    })}
-                </div>
-                <div className="slider__arrow-right">
-                    <span
-                        className="arrow"
-                        onClick={() => {
-                            document.getElementById(id).scrollLeft += window.innerWidth - 80
-                        }}
-                    >
-                        <ArrowBackIosIcon />
-                    </span>
-                </div>
-            </div>
-            {modalVisibility && (
-                <div></div>
-            )}
-        </section>
-    );
-}
+  const handleClick = (movie) => {
+    setModalVisibility(true);
+    setMovieSelection(movie);
+  };
+  return (
+    <section className="row">
+      <h2>{title}</h2>
+      <div className="slider">
+        <div className="slider__arrow-left">
+          <span
+            className="arrow"
+            onClick={() => {
+              document.getElementById(id).scrollLeft -= window.innerWidth - 80;
+            }}
+          >
+            <ArrowBackIosIcon />
+          </span>
+        </div>
+        <div id={id} className="row__posters">
+          {/**SEVERAL ROW__POSTER */}
+          {movies.map((movie, idx) => (
+            <img
+              key={idx}
+              onClick={() => handleClick(movie)}
+              className={`row__poster row__posterLarge`}
+              src={`${base_url}${movie.poster_path}`}
+              loading="lazy"
+              alt={movie.title}
+            />
+          ))}
+        </div>
+        <div className="slider__arrow-right">
+          <span
+            className="arrow"
+            onClick={() => {
+              document.getElementById(id).scrollLeft += window.innerWidth - 80;
+            }}
+          >
+            <ArrowForwardIosIcon />
+          </span>
+        </div>
+      </div>
+      {modalVisibility && (
+        <MovieModal
+          {...movieSelected}
+          setModalVisibility={setModalVisibility}
+        />
+      )}
+    </section>
+  );
+};
 
 export default Row;
