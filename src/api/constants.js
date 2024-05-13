4a22501289d4fcb1e0577a5cc30c6a81712f65eb
Chandra Lama <iamme24cl@gmail.com>
2024-05-13 13:52:48 -0700

    .

--- src/api/constants.js
+++ src/api/constants.js
@@ -1,2 +1,2 @@
-// export const baseUrl = "https://movie-lan.fly.dev";
-export const baseUrl = "http://127.0.0.1:8000"
+export const baseUrl = "https://movie-lan.fly.dev";
+// export const baseUrl = "http://127.0.0.1:8000"
